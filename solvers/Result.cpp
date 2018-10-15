90b7ce53c627b68da99e2028226bfae8c0562a5b
bachnx <thanbaiks@gmail.com>
2018-10-15 23:38:48 +0700

    WIP

--- solvers/Result.cpp
+++ solvers/Result.cpp
@@ -5,19 +5,5 @@ solver::Result::Result() {}
 solver::Result::Result(TestCase* testcase) : origin(testcase->origin), basePath(testcase->basePath) {}
 
 void solver::Result::visualize() {
-    using cimg_library::CImg;
-    using cimg_library::CImgDisplay;
-    CImg<unsigned char> frame((this->basePath + "/" + this->origin).data());
-    frame.normalize(0, 64);
-    for (auto &part : this->items) {
-        CImg<unsigned char> imgPart((this->basePath + "/" + part.name).data());
-        if (part.r) {
-            imgPart.rotate(part.r, 0, 0);
-        }
-        frame.draw_image(part.x, part.y, imgPart);
-    }
-    CImgDisplay display(frame, "Visualized", 0);
-    while (!display.is_closed()) {
-        display.wait();
-    }e
+
 }
