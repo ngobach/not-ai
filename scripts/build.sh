3b2b94d0a5e126d00b5a19bbcb3bfda1577e7e22
bachnx <thanbaiks@gmail.com>
2018-10-21 22:29:03 +0700

    Restruct done

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+
 SRCDIR=$(pwd)
-cd $SRCDIR/../build && cmake -D CMAKE_BUILD_TYPE=Release $SRCDIR && make
+for lf in libopencv_core libopencv_highgui libopencv_imgcodecs libopencv_imgproc; do
+	ln -sf ${lf}.so.3.4.3 $SRCDIR/opencv/lib/${lf}.so
+	ln -sf ${lf}.so.3.4.3 $SRCDIR/opencv/lib/${lf}.so.3.4
+	ln -sf ${lf}.so.3.4.3 $SRCDIR/opencv/lib/${lf}.so.3
+done
+
+# Building
+cd $SRCDIR/../build
+cmake -D CMAKE_BUILD_TYPE=Release $SRCDIR &> /dev/null
+make &> /dev/null
 cp $SRCDIR/run.sh $SRCDIR/../build/run.sh
+
